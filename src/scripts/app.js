ae5993d9ad98b4ee41fe87881900da453215a295
jonathandavidblack <jonnybl915@gmail.com>
2016-11-02 21:01:07 -0400

    added HomeView and main render functions and input field

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,31 @@
-const Backbone = require('Backbone')
+const ReactDOM = require('react-dom');
+const React = require('react');
+const Backbone = require('backbone');
+var $ = require('jquery');
+console.log("React Object: ", React);
 
-console.log(Backbone)
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+
+const HomeView = React.createClass({
+
+
+  render: function(){
+    return (
+      <div className="todo-container">
+        <form className="col s12" id="todo-form-container">
+          <div className="row">
+             <div className="input-field col s6">
+               <input placeholder="Placeholder" id="first_name" type="text" className="validate todo-input"></input>
+               <label className="todo-input" ></label>
+             </div>
+           </div>
+        </form>
+      </div>
+
+
+
+
+
+    )
+  }
+})
+ReactDOM.render(<HomeView/>, document.querySelector('#app-container'))
